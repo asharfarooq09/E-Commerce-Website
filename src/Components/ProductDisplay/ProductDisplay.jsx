d0f217a4d06960290e24920147852e7bb85f01a8
Ashar <farooqashar09@gmail.com>
2025-01-28 17:15:29 +0530

    second commit

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,5 +1,5 @@
 import React, { useContext} from "react";
-import "./ProductDisplay.css";
+import "./productDisplay.css";
 import star_icon from "../assets/star_icon.png";
 import star_dull_icon from "../assets/star_dull_icon.png";
 import { ShopContext } from "../../Context/ShopContext";
